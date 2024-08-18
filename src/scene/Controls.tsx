32deb38ddaf6bc520a45f9bc01ab02a245669a40
Brooklyn <brooklyn.bb.nicholson@gmail.com>
2024-08-18 14:41:21 -0500

    chore: uptick

--- src/scene/Controls.tsx
+++ src/scene/Controls.tsx
@@ -1,3 +1,5 @@
+'use client'
+
 import { OrbitControls } from '@react-three/drei'
 import { useEffect, useState } from 'react'
 
@@ -8,9 +10,14 @@ export default function Controls() {
     const ac = new AbortController()
     const { signal } = ac
 
+    const handle = () => set(window.innerWidth <= 1024)
+
     ;(['keyup', 'keydown'] as const).forEach(evt =>
       window.addEventListener(evt, e => set(e.altKey), { signal })
     )
+
+    window.addEventListener('resize', handle, { signal })
+    window.requestAnimationFrame(handle)
 
     return () => ac?.abort()
   }, [])
